2d439fe23549437178b746c50eedf155e0bcef28
Birbola <jotanazarov5159@gmail.com>
2022-03-31 19:24:16 +0500

    sa

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const ism: HTMLInputElement = document.querySelector('#ism')!
+const familiya: HTMLInputElement = document.querySelector('#familiya')!
+const email: HTMLInputElement = document.querySelector('#email')!
+const password: HTMLInputElement = document.querySelector('#password')!
+const tugma: HTMLInputElement = document.querySelector('#tugma')!
